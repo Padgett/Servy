858c42667f5825193a1a131f331d79741f353eb8
Joshua Padgett <1054549+Padgett@users.noreply.github.com>
2024-11-28 20:51:19 -0700

    Add About Us page

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -70,9 +70,28 @@ defmodule Servy.Handler do
     %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
   end
 
+  def route(%{ method: "GET", path: "/about" } = conv) do
+    Path.expand("../../pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read
+    |> handle_file(conv)
+  end
+
   # Catch all - 404
   def route(%{ path: path } = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  defp handle_file({:ok, content}, conv) do
+    %{ conv | status: 200, resp_body: content }
+  end
+
+  defp handle_file({:error, :enoent}, conv) do
+    %{ conv | status: 404, resp_body: "File not found" }
+  end
+
+  defp handle_file({:error, reason}, conv) do
+    %{ conv | status: 500, resp_body: "File Error: #{reason}" }
   end
 
   def format_response(conv) do
@@ -171,6 +190,18 @@ IO.puts response
 
 request = """
 GET /lions?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
