eaa743bccf1a1c824120718b332471406463fbde
Joshua Padgett <1054549+Padgett@users.noreply.github.com>
2024-11-30 19:35:06 -0700

    Introduce Conv module for request handling and refactor handler to use Conv struct

--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -1,33 +1,35 @@
 defmodule Servy.Plugins do
   require Logger
 
-  def track(%{status: 404, path: path} = conv) do
+  alias Servy.Conv
+
+  def track(%Conv{status: 404, path: path} = conv) do
     Logger.info "404 -  #{path} is on the loose!"
     conv
   end
 
-  def track(%{status: 403, path: path, method: method} = conv) do
+  def track(%Conv{status: 403, path: path, method: method} = conv) do
     Logger.warning "403 - Attempted #{method} of #{path}"
     conv
   end
 
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
     %{ conv | path: "/wildthings" }
   end
 
-  def rewrite_path(%{path: path} = conv) do
+  def rewrite_path(%Conv{path: path} = conv) do
     regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
     captures = Regex.named_captures(regex, path)
     rewrite_path_captures(conv, captures)
   end
 
-  def rewrite_path(conv), do: conv
+  def rewrite_path(%Conv{} = conv), do: conv
 
-  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+  defp rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
     %{ conv | path: "/#{thing}/#{id}" }
   end
 
-  def rewrite_path_captures(conv, nil), do: conv
+  defp rewrite_path_captures(conv, nil), do: conv
 end
